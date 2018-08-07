4dfb2761fdba0bea5306c974ead9469207142743
Janderyang <42180630+Janderyang@users.noreply.github.com>
2018-08-07 23:19:13 +0800

    上次文件
    学习上传文件

---
+++ season_health.py
@@ -0,0 +1,43 @@
+import pymysql
+import requests
+from bs4 import BeautifulSoup
+
+baseUrl = "http://care.39.net/sjbj/xjbj/xrys/index_%d.html"
+def get_inform(start):
+	url = baseUrl % start
+	lists = []
+	html = requests.get(url)
+	soup = BeautifulSoup(html.content, "html.parser")
+	items = soup.find_all("span", "text")
+	for i in items:
+		ys = {}
+		ys["title"] = i.find("a").text
+		ys["link"] = i.find("a").get("href")
+		#print(ys["title"])
+		lists.append(ys)
+	return lists
+
+if __name__ == "__main__":
+	db = pymysql.connect(host="localhost",user="root",password="1234",db="health",charset="utf8")
+	cursor = db.cursor()
+	cursor.execute("DROP TABLE IF EXISTS ys")
+	createTab = """CREATE TABLE ys(
+		id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		title VARCHAR(50) NOT NULL,
+		link VARCHAR(50) NOT NULL
+	)"""
+	cursor.execute(createTab)
+	start = 1
+	while (start < 15):
+		lists = get_inform(start)
+		for i in lists:
+			sql = "INSERT INTO `ys`(`title`,`link`) VALUES(%s,%s)"
+			try:
+				cursor.execute(sql, (i["title"],i["link"]))
+				db.commit()
+			except:
+				#print("faild")
+				db.rollback()
+		start += 1
+		#print("finsh")
+	db.close()
